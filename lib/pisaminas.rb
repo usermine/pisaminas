b40ceec57710d184b9be3e5479c617c79cc0b595
kleer <kleer@kleer-csd-box>
2017-06-14 11:36:11 -0500

    TDD

---
+++ lib/pisaminas.rb
@@ -0,0 +1,14 @@
+class Pisaminas
+    
+	def marcarPos pos
+		bomba = "A3"
+		if (pos==bomba)
+			var = "perdiste"
+		elsif 
+			var ="avanzar"
+		end
+
+		return var
+	end
+
+end
---
+++ spec/pisaminas_spec.rb
@@ -0,0 +1,18 @@
+
+require "./lib/pisaminas.rb"
+
+describe Pisaminas do
+	it "Juego 1" do
+		pisaminas = Pisaminas.new
+		result = pisaminas.marcarPos("A1")
+		expect(result).to eq "avanzar"
+	end
+
+	it "Juego 2" do
+		pisaminas = Pisaminas.new
+		result = pisaminas.marcarPos("A3")
+
+		expect(result).to eq "perdiste"
+	end
+
+end
