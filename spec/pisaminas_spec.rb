a855847b803b2f6767aa6f60d09647899c035b2c
Johan Prieto <jheimar@gmail.com>
2017-06-14 12:14:03 -0500

    Version funcional pisaminas v 1.0

--- spec/pisaminas_spec.rb
+++ spec/pisaminas_spec.rb
@@ -4,15 +4,15 @@ require "./lib/pisaminas.rb"
 describe Pisaminas do
 	it "Juego 1" do
 		pisaminas = Pisaminas.new
-		result = pisaminas.marcarPos("A1")
+		result = pisaminas.marcarPos("A3")
 		expect(result).to eq "avanzar"
 	end
 
 	it "Juego 2" do
 		pisaminas = Pisaminas.new
-		result = pisaminas.marcarPos("A3")
+		result = pisaminas.marcarPos("A1")
 
-		expect(result).to eq "mina"
+		expect(result).to eq "perdiste"
 	end
 
 end
